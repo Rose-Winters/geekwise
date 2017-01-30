f4036269f91addbdc6fd3562e09163dd28d7f89c
student-06 <student-06>
2017-01-30 15:50:15 -0800

    start on javascript

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,28 @@
+// alert('this is an alert');
+
+/*
+  comment
+  out
+  multiple
+  lines
+*/
+
+var myNumberVar = 7, //Number Object
+    myFloatVar = 5.2, //Float Object
+    myStringVar = "Matthew Higley", //String Object
+    myBooleanVar = true, //Boolean Objectt
+    myArrayVar = ['one', 'stingray', 'banana', 8, true], // Array Object
+    myObjectVar = {
+      make: 'BMW',
+      model: '328i',
+      color: 'red',
+      year: 2006
+    }; //Object Object
+
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myArrayVar);
+console.log( myObjectVar);
