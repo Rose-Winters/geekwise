0ad5f98ff750196058c569707170c1941c6e10ac
student-06 <student-06>
2017-02-07 12:35:23 -0800

    create a story with javascript Feb06

---
+++ feb06-js-numbers/js/script.js
@@ -0,0 +1,59 @@
+var myString = 'This is a string, a name & pizza',
+    myNumber = 5,
+    myBool = true,
+    myFloat = 5.2,
+    myArray = ['red', 8, false, true, 'chef boyardee'],
+    myObject = {
+      spaghetti: true,
+      noodles: false,
+      ramen: true,
+      ravioli: 'show me the formuoli',
+      eight: 8
+    };
+// console.log(myBool);
+// console.log(myString.indexOf('i', 3));
+// console.log(myArray[4]);
+// console.log(myObject.ravioli);
+
+//Number Object
+
+var numOne = 2,
+    numTwo = new Number(5);
+
+/*
+console.log(numOne);
+console.log(numTwo);
+*/
+
+var numThree = '3',
+    numFour = '5.2';
+// console.log(parseInt(numThree) );
+// console.log(parseFloat(numFour));
+
+var numFive = 'Rosa';
+// console.log( parseFloat(numFive));
+// console.log( parseInt(numFive));
+// if( isNaN(numFive) ){
+//     alert('its not a number');}
+
+// parseInt
+//parseFloat
+//isNaN
+
+// alert('What did you say?');
+// var userLang = confirm('Are you alive?');
+// var userQuestion = prompt('What is your favorite color?');
+// console.log(userQuestion);
+// console.log(userLang);
+
+
+// var firstName = prompt('what is your first name?'),
+//     secondName = prompt('what is your middle name?'),
+//     lastName = prompt('what is your last name'),
+//     concatenate = firstName + ' ' + secondName + ' ' + lastName;
+// console.log(concatenate);
+
+
+
+// var ageYear = prompt('In what year will you be 21?');
+// console.log(parseInt(ageYear));,
